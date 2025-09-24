4e8fc83addf17b6838ff864ec00292478a6da639
Scott Gallagher-Starr <scott.gallagherstarr@gmail.com>
2025-09-23 20:41:40 -0700

    Nailed it! The NavigationRail entries for Layouts and Random are both finished. I've removed the PromptedChoice items from the command buttons panel as well, and all works as it shoudl.

--- lib/ui_layer/top_level_layout/main_navigation_rail.dart
+++ lib/ui_layer/top_level_layout/main_navigation_rail.dart
@@ -16,6 +16,43 @@ class _MainNavigationRailState extends State<MainNavigationRail> {
   bool showTrailing = false;
   double groupAlignment = -1.0;
 
+  Widget _buildTarotLayoutChoiceChip(
+    BuildContext context, {
+    required Signal<TarotLayout> signal,
+    required String key,
+  }) => Watch(
+    (BuildContext context) => ChoiceChip(
+      label: Text(key),
+      selected: signal.value.displayName == key,
+      onSelected: (bool selected) {
+        if (selected) {
+          signal.value = sl<LayoutManager>().getLayoutByDisplayName(key);
+        }
+      },
+    ),
+  );
+
+  Future<void> _tarotLayoutsDialogBuilder(BuildContext context) => showDialog(
+    context: context,
+    builder: (BuildContext context) => Watch(
+      (BuildContext context) => AlertDialog(
+        title: Text(
+          "Select a Tarot Layout",
+          style: Theme.of(context).textTheme.headlineSmall,
+        ),
+        actions: ComputedsManager.layoutDisplayNames.value
+            .map(
+              (e) => _buildTarotLayoutChoiceChip(
+                context,
+                signal: SignalsManager.tarotLayout,
+                key: e,
+              ),
+            )
+            .toList(),
+      ),
+    ),
+  );
+
   Future<void> _randomSourceDialogBuilder(BuildContext context) => showDialog(
     context: context,
     builder: (BuildContext context) => AlertDialog(
@@ -27,7 +64,7 @@ class _MainNavigationRailState extends State<MainNavigationRail> {
         int index,
       ) {
         return Watch(
-          (BuildContext build) => ChoiceChip(
+          (BuildContext context) => ChoiceChip(
             label: Text(RandomGenerators.values[index].displayName),
             selected:
                 SignalsManager.currentRandomGenerator.value.index == index,
@@ -49,14 +86,6 @@ class _MainNavigationRailState extends State<MainNavigationRail> {
       onDestinationSelected: (int index) {
         setState(() {
           _selectedIndex = index;
-
-          // switch (_selectedIndex) {
-          //   case 3:
-          //     Navigator.of(context).restorablePush(_randomSourceDialogBuilder);
-          //   // Navigator.of(context).push(RandomSourcePopupMain());
-          //   case _:
-          //     ;
-          // }
         });
       },
       labelType: labelType,
@@ -89,9 +118,26 @@ class _MainNavigationRailState extends State<MainNavigationRail> {
           label: Text('Second'),
         ),
         NavigationRailDestination(
-          icon: Badge(label: Text('4'), child: Icon(Icons.star_border)),
-          selectedIcon: Badge(label: Text('4'), child: Icon(Icons.star)),
-          label: Text('Third'),
+          icon: IconButton(
+            onPressed: () {
+              _tarotLayoutsDialogBuilder(context);
+            },
+            icon: Badge(
+              label: Watch(
+                (BuildContext context) =>
+                    Text(SignalsManager.tarotLayout.value.displayName),
+              ),
+              child: Icon(LucideIcons.layout_dashboard),
+            ),
+            selectedIcon: Badge(
+              label: Watch(
+                (BuildContext context) =>
+                    Text(SignalsManager.tarotLayout.value.displayName),
+              ),
+              child: Icon(LucideIcons.layout_dashboard),
+            ),
+          ),
+          label: Text('Layouts'),
         ),
         NavigationRailDestination(
           icon: IconButton(
@@ -107,93 +153,22 @@ class _MainNavigationRailState extends State<MainNavigationRail> {
               child: Icon(LucideIcons.dices),
             ),
           ),
-          selectedIcon: Badge(
-            label: Watch(
-              (BuildContext context) =>
-                  Text(SignalsManager.currentRandomGenerator.value.displayName),
+          selectedIcon: IconButton(
+            onPressed: () {
+              _randomSourceDialogBuilder(context);
+            },
+            icon: Badge(
+              label: Watch(
+                (BuildContext context) => Text(
+                  SignalsManager.currentRandomGenerator.value.displayName,
+                ),
+              ),
+              child: Icon(LucideIcons.dices),
             ),
-            child: Icon(LucideIcons.dices, color: Colors.green),
           ),
           label: Text("Randomness"),
         ),
       ],
-      //         ),
-      //         const VerticalDivider(thickness: 1, width: 1),
-      //         // This is the main content.
-      //         Expanded(
-      //           child: Column(
-      //             mainAxisAlignment: MainAxisAlignment.center,
-      //             children: <Widget>[
-      //               Text('selectedIndex: $_selectedIndex'),
-      //               const SizedBox(height: 20),
-      //               Text('Label type: ${labelType.name}'),
-      //               const SizedBox(height: 10),
-      //               SegmentedButton<NavigationRailLabelType>(
-      //                 segments: const <ButtonSegment<NavigationRailLabelType>>[
-      //                   ButtonSegment<NavigationRailLabelType>(
-      //                     value: NavigationRailLabelType.none,
-      //                     label: Text('None'),
-      //                   ),
-      //                   ButtonSegment<NavigationRailLabelType>(
-      //                     value: NavigationRailLabelType.selected,
-      //                     label: Text('Selected'),
-      //                   ),
-      //                   ButtonSegment<NavigationRailLabelType>(
-      //                     value: NavigationRailLabelType.all,
-      //                     label: Text('All'),
-      //                   ),
-      //                 ],
-      //                 selected: <NavigationRailLabelType>{labelType},
-      //                 onSelectionChanged:
-      //                     (Set<NavigationRailLabelType> newSelection) {
-      //                       setState(() {
-      //                         labelType = newSelection.first;
-      //                       });
-      //                     },
-      //               ),
-      //               const SizedBox(height: 20),
-      //               Text('Group alignment: $groupAlignment'),
-      //               const SizedBox(height: 10),
-      //               SegmentedButton<double>(
-      //                 segments: const <ButtonSegment<double>>[
-      //                   ButtonSegment<double>(value: -1.0, label: Text('Top')),
-      //                   ButtonSegment<double>(value: 0.0, label: Text('Center')),
-      //                   ButtonSegment<double>(value: 1.0, label: Text('Bottom')),
-      //                 ],
-      //                 selected: <double>{groupAlignment},
-      //                 onSelectionChanged: (Set<double> newSelection) {
-      //                   setState(() {
-      //                     groupAlignment = newSelection.first;
-      //                   });
-      //                 },
-      //               ),
-      //               const SizedBox(height: 20),
-      //               SwitchListTile(
-      //                 title: Text(showLeading ? 'Hide Leading' : 'Show Leading'),
-      //                 value: showLeading,
-      //                 onChanged: (bool value) {
-      //                   setState(() {
-      //                     showLeading = value;
-      //                   });
-      //                 },
-      //               ),
-      //               SwitchListTile(
-      //                 title: Text(
-      //                   showTrailing ? 'Hide Trailing' : 'Show Trailing',
-      //                 ),
-      //                 value: showTrailing,
-      //                 onChanged: (bool value) {
-      //                   setState(() {
-      //                     showTrailing = value;
-      //                   });
-      //                 },
-      //               ),
-      //             ],
-      //           ),
-      //         ),
-      //       ],
-      //     ),
-      //   ),
     );
   }
 }
